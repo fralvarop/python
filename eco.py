8800d0381d2f546e59df26e7c690b1e08ee40727
Raul Lopez <lopecillo@gmail.com>
2012-05-17 21:02:40 +0200

    mi segundo commit

--- eco.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-while True:
-    respuesta = raw_input('Escribe algo ("adiós" para salir): ')
-    if respuesta == 'adiós':
-        print 'Adiós, gilipollas'
-        break
-    else:
-        print '¡Eco! %s' % respuesta
